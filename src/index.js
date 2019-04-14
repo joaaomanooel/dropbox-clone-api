1753287e8f210952a6061320b3918963f085ceb7
João Manoel Neto <joaaomanooel@gmail.com>
2019-04-13 23:55:02 -0300

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const socket = require('socket.io');
+const { Server } = require('http');
 
 const app = express();
-mongoose.connect('mongodb+srv://dropbox-clone-api:dropbox-clone-api123@cluster0-df4ak.mongodb.net/dropbox?retryWrites=true', {
+const server = Server(app);
+const io = socket(server);
+
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
 });
+
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '../tmp')));
 app.use('/api/v1', require('./routes'));
 
-module.exports = app;
+module.exports = server ;
